4b0785e4ac34f4a314423e3adc82625b62de0180
Tanguy <tanguy.folliot@epitech.eu>
2026-02-16 22:48:57 +0100

    feat: add the server file to launch the api

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+require('dotenv').config();
+
+
+const authRoutes = require('./routes/auth');
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.use('/auth', authRoutes);
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
