4ed2da4dc9263c3dd405586d6f7763cd5212a38f
Tanguy <tanguy.folliot@epitech.eu>
2026-02-17 13:18:53 +0100

    feat (frontend): add the react app

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Homes";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
